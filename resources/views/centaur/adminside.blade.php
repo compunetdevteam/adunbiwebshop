9da0882e0dc632232fbcca668592394a054049a5
joeizang <joeizang@hotmail.com>
2016-08-16 11:57:11 +0100

    Changes to links on the dashboard

--- resources/views/centaur/adminside.blade.php
+++ resources/views/centaur/adminside.blade.php
@@ -12,13 +12,13 @@
             <a href="javascript:;" data-toggle="collapse" data-target="#demo3"><i class="fa fa-user"></i> Staff <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="demo3" class="collapse">
                 <li>
-                    <a href="createstaff">Create Staff</a>
+                    <a href="{{url('register')}}">Create Staff</a>
                 </li>
                 <li>
-                    <a href="viewstaff">View Staff</a>
+                    <a href="{{url('users')}}">View Staff</a>
                 </li>
                 <li>
-                    <a href="assignrole">Assign Staff Role</a>
+                    <a href="{{url('roles')}}">Assign Staff Role</a>
                 </li>
             </ul>
 
@@ -27,7 +27,7 @@
             <a href="javascript:;" data-toggle="collapse" data-target="#demo4"><i class="fa fa-fw fa-table"></i> Customer <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="demo4" class="collapse">
                 <li>
-                    <a href="viewcustomer">View Customers</a>
+                    <a href="{{url('sales/viewcustomers')}}">View Customers</a>
                 </li>
 
             </ul>
@@ -37,12 +37,12 @@
             <a href="javascript:;" data-toggle="collapse" data-target="#demo5"><i class="fa fa-fw fa-list-alt"></i> Products <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="demo5" class="collapse">
                 <li>
-                    <a href="products/create">Create Products</a>
+                    <a href="{{url('products/create')}}">Create Products</a>
                 </li>
                 <li>
-                    <a href="products">View Products</a>
+                    <a href="{{url('products')}}">View Products</a>
                 </li>
-                <li><a href="products/edit">Update Product</a></li>
+                <li><a href="{{url('products/edit')}}">Update Product</a></li>
             </ul>
 
         </li>
@@ -50,14 +50,14 @@
             <a href="javascript:;" data-toggle="collapse" data-target="#demo6"><i class="fa fa-fw fa-shopping-cart"></i> Sales <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="demo6" class="collapse">
                 <li>
-                    <a href="sales">View Sales</a>
+                    <a href="{{url('sales')}}">View Sales</a>
                 </li>
                 <li>
-                    <a href="sales/create">Make A Sale</a>
+                    <a href="{{url('sales/create')}}">Make A Sale</a>
                 </li>
 
                 <li>
-                    <a href="setdiscount">Set Discount</a>
+                    <a href="{{url('sales/setdiscount')}}">Set Discount</a>
                 </li>
 
             </ul>
@@ -66,10 +66,10 @@
             <a href="javascript:;" data-toggle="collapse" data-target="#demo7"><i class="fa fa-fw fa-briefcase"></i> Stocks<i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="demo7" class="collapse">
                 <li>
-                    <a href="viewstock">View Stocks</a>
+                    <a href="{{url('stock/viewstock')}}">View Stocks</a>
                 </li>
                 <li>
-                    <a href="urnewarrivals">New Arrivals</a>
+                    <a href="{{url('stock/newarrivals')}}">New Arrivals</a>
                 </li>
 
             </ul>
